dd0139f8402d1af9ff5b19691125dc8cc4d042c4
JohnnyBC2022 <jonathanbc2012@gmail.com>
2025-05-15 13:54:42 +0200

    Add: new exercise

---
+++ Operadores/operadores_asignacion.py
@@ -0,0 +1,15 @@
+# Operadores de asignación
+
+print('*** Operadores de Asignación ***')
+numero = 5
+print(f'Valor de a: {numero}')
+cadena = 'Saludos desde Python'
+print(f'Valor de cadena: {cadena}')
+
+# Crear múltiples variables en una sola línea
+a, b, c = 10, 'Saludos', 14.5
+print(f'Valores de a: {a}, b: {b}, c: {c}')
+
+# Asignar un mismo valor a varias variables
+x = y = z = 10
+print(f'Valor de x: {x}, y: {y}, z:{z}')
