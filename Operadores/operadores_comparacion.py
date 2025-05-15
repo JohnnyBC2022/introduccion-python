a9cd67ac672faa1e2416b5640ceea3741bca5c6c
JohnnyBC2022 <jonathanbc2012@gmail.com>
2025-05-15 14:15:49 +0200

    Add: new exercise

---
+++ Operadores/operadores_comparacion.py
@@ -0,0 +1,27 @@
+print('*** Operadores de comparación (relacionales) ***')
+a, b = 7, 5
+print(f'Valor inicial a: {a}, b: {b}')
+
+# Operador de igualdad == Devuelve un valor lógico (True/False)
+resultado = a == b  # False
+print(f'Resultado a == b es: {resultado}')
+
+# Operador distinto !=
+resultado = a != b  # True
+print(f'Resultado a != b es: {resultado}')
+
+# Operador mayor que >
+resultado = a > b  # True
+print(f'Resultado a > b es: {resultado}')
+
+# Operador mayor o igual que >=
+resultado = a >= b  # True
+print(f'Resultado a >= b es: {resultado}')
+
+# Operador menor que <
+resultado = a < b  # False
+print(f'Resultado a < b es: {resultado}')
+
+# Operador menor o igual que <=
+resultado = a <= b  # False
+print(f'Resultado a <= b es: {resultado}')
---
+++ Operadores/operadores_logicos.py
@@ -0,0 +1,10 @@
+from operadores_comparacion import resultado
+
+print('*** Operador AND ***')
+
+# Devuelve True si ambos valores son verdaderos
+condicion1 = False
+condicion2 = False
+
+resultado = condicion1 and condicion2
+print(f'Resultado {condicion1} y {condicion2} es: {resultado}')
