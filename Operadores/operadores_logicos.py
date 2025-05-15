69b6d1d09e6b11670dddd9974e806397812079aa
JohnnyBC2022 <jonathanbc2012@gmail.com>
2025-05-15 14:29:13 +0200

    Update: add OR operator example

--- Operadores/operadores_logicos.py
+++ Operadores/operadores_logicos.py
@@ -1,10 +1,16 @@
-from operadores_comparacion import resultado
-
 print('*** Operador AND ***')
 
 # Devuelve True si ambos valores son verdaderos
-condicion1 = False
+condicion1 = True
 condicion2 = False
 
 resultado = condicion1 and condicion2
 print(f'Resultado {condicion1} y {condicion2} es: {resultado}')
+
+print('*** Operador OR ***')
+# Devuelve True si cualquiera de los valores es verdadero
+condicion3 = True
+condicion4 = False
+
+resultado = condicion3 or condicion4
+print(f'Resultado {condicion3} y {condicion4} es: {resultado}')
