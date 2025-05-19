9e81bc40507bf39daed3c3eb18b300a5c1a7e5e3
JohnnyBC2022 <jonathanbc2012@gmail.com>
2025-05-19 20:12:59 +0200

    Add new exercise

---
+++ SentenciasDecision/02_sentencias_multiples.py
@@ -0,0 +1,11 @@
+print('*** Sentencias Decisión Múltiples ***')
+
+dia_con_lluvia = True
+dia_nublado = False
+
+if dia_con_lluvia:
+    print('Llevar paraguas')
+elif dia_nublado:
+    print('Llevar chubasquero')
+else:
+    print('Dejar paraguas y chubasquero en casa')
