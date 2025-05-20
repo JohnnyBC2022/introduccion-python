9fbff7161f3571cbcc693cb112d92d5e3dc6c07a
JohnnyBC2022 <jonathanbc2012@gmail.com>
2025-05-20 20:08:38 +0200

    Add new exercise

---
+++ SentenciasDecision/06_mayor_dos_numeros.py
@@ -0,0 +1,9 @@
+print('*** El mayor de dos números ***')
+
+numero1 = int(input('Escribe el número 1: '))
+numero2 = int(input('Escribe el número 2: '))
+
+if numero1 > numero2:
+    print(f'El número {numero1} es mayor')
+else:
+    print(f'El número {numero2} es mayor')
