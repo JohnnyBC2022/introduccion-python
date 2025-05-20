5e7d87246051b5f0d696f1ca710a8dd8c1ad382d
JohnnyBC2022 <jonathanbc2012@gmail.com>
2025-05-20 20:17:04 +0200

    Exercise updated with ternary operator

--- SentenciasDecision/06_mayor_dos_numeros.py
+++ SentenciasDecision/06_mayor_dos_numeros.py
@@ -3,7 +3,14 @@ print('*** El mayor de dos números ***')
 numero1 = int(input('Escribe el número 1: '))
 numero2 = int(input('Escribe el número 2: '))
 
+# El mayor de dos números
 if numero1 > numero2:
     print(f'El número {numero1} es mayor')
 else:
     print(f'El número {numero2} es mayor')
+
+# Sintaxis alternativa con operador ternario (cambia respecto a Javascript)
+# Javascript: condicion ? codigo_true : codigo_false
+# Python: codigo_true if condicion else codigo_false
+
+print(f'El número {numero1} es mayor') if numero1 > numero2 else print(f'El número {numero2} es mayor')
