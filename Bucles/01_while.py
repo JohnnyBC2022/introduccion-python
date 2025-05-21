d20b7a44451e39a580445a8df16dddb38ad1c014
JohnnyBC2022 <jonathanbc2012@gmail.com>
2025-05-21 20:03:43 +0200

    Add new exercise

---
+++ Bucles/01_while.py
@@ -0,0 +1,10 @@
+print('*** Bucle While ***')
+
+# Imprimir del 1 al 5
+contador = 1
+VALOR_MAXIMO = 5
+
+# Ejecutamos el ciclo while
+while contador <= VALOR_MAXIMO:
+    print(f'Contador: {contador}')
+    contador += 1  # contador = contador + 1
