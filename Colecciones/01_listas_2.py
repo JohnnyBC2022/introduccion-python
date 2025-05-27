30d5f6c93d390d664dc1c6a7a463b92ffcf1f5c8
JohnnyBC2022 <jonathanbc2012@gmail.com>
2025-05-27 21:00:47 +0200

    Add new exercise

---
+++ Colecciones/01_listas_2.py
@@ -0,0 +1,14 @@
+print('*** Manejo de Listas (Continuación) ***')
+
+nombres = ['Karla', 'Juan', 'Laura']
+
+# Iterar sobre una lista
+for nombre in nombres:
+    print(nombre)
+
+print('\n')
+
+lista_heterogenea = [100, True, 'Pepe']
+
+for elemento in lista_heterogenea:
+    print(elemento)
